099c505d05a8410cddb6ffb779befa31d0dea8cf
skrodrigo <rodrigoa0987@gmail.com>
2024-05-28 12:02:04 -0300

    tipando a classe de criar

--- src/developers/dto/create-developer.dto.ts
+++ src/developers/dto/create-developer.dto.ts
@@ -1,4 +1,13 @@
+import { IsDateString, IsEmail, IsString } from "class-validator";
+
 export class CreateDeveloperDto {
 
-    
+    @IsString()
+    name: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsDateString()
+    dateOfBirth: string;
 }
