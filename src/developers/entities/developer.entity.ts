9e38e07681ec70139640b1f4ec551c3a8e198013
skrodrigo <rodrigoa0987@gmail.com>
2024-05-28 12:17:13 -0300

    create information to table db

--- src/developers/entities/developer.entity.ts
+++ src/developers/entities/developer.entity.ts
@@ -1,1 +1,23 @@
-export class Developer {}
+import { BeforeInsert, Column, Entity, PrimaryColumn } from "typeorm";
+
+import { nanoid } from 'nanoid';
+    
+@Entity('developers')
+export class Developer {
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    dateOfBirth: string;
+    
+    @BeforeInsert()
+    generateId() {
+        this.id = `dev_${nanoid()}`;
+    }
+}
